ccb05630e7613aed8eb9255c69e26a17913caa78
cyc <2477721334@qq.com>
2023-08-22 13:01:43 +0800

    go语言爬取哔哩哔哩标题、详细信息

--- fmt/fmt.go
+++ fmt/fmt.go
@@ -1,5 +1,17 @@
 package fmt
 
-func init() {
+import (
+	"io"
+	"log"
+	"os"
+)
 
+var Logger *log.Logger
+
+func init() {
+	file, error := os.OpenFile("trace.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+	if error != nil {
+		panic(error)
+	}
+	Logger = log.New(io.MultiWriter(os.Stdout, file), "cyc Log:", log.LstdFlags)
 }
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	downloader "awesomeProject/downloader"
+	myfmt "awesomeProject/fmt"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	request := downloader.InfoRequest{Bvids: []string{"BV1Jo4y1y7yZ", "BV1184y1o7yP", "BV1Yx4y1X7Kg"}}
+	response, err := downloader.BatchDownloadVideoInfo(request)
+	if err != nil {
+		panic(err)
+	}
+	for _, info := range response.Infos {
+		myfmt.Logger.Printf("title:%s \ndesc:%s\n", info.Data.Title, info.Data.Desc)
+	}
 }
